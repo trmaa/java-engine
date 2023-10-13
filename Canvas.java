607954ab3830fece32cc24a5cae38a03c262b573
Pablo <trmaayt@gmail.com>
2023-10-13 10:42:19 +0200

    java engine

---
+++ Canvas.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+
+public class Canvas extends JPanel {
+    public MyKeyListener kl = new MyKeyListener();
+
+    public Canvas() {
+        this.addKeyListener(this.kl);
+        this.setLayout(new BorderLayout());
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        this.cls(g, new Color(0x449911));
+    }
+
+    public void cls(Graphics g, Color c) {
+        g.setColor(c);
+        g.fillRect(0, 0, Main.ventana.getWidth(), Main.ventana.getHeight());
+    }
+
+    public void print(Graphics g, double x, double y, double w, double h, Color c) {
+        g.setColor(c);
+        g.fillRect(Utils.redondear(x), Utils.redondear(y), Utils.redondear(w), Utils.redondear(h));
+    }
+
+    public void printext(Graphics g, String msg, int x, int y, String fnt, int d, Color c) {
+        g.setColor(c);
+        g.setFont(new Font(fnt, Font.BOLD, d));
+        g.drawString(msg, x, y);
+    }
+}
